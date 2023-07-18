7885e3e77add6ed93ded6bfd77e8af2e56c0d21a
rama.sahoo <rama.sahoo@gmail.com>
2023-07-18 06:37:51 +0530

    test11

--- login.py
+++ login.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+option=webdriver.ChromeOptions()
+option.headless=True
+driver=webdriver.Chrome(executable_path=ChromeDriverManager().install(),options=option)
 driver.get("https://demo.opencart.com/admin/")
 # op=driver.title
 # assert (op,"Administration")
