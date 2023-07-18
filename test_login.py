36b826c741617b900198f553f834e7942155a053
ramagihub <32742931+ramagihub@users.noreply.github.com>
2023-07-18 19:33:39 +0530

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-driver.get("https://demo.opencart.com/admin/")
-op=driver.title
-assert op=="Administration",'mismatched title'
+def test_1():
+  options = webdriver.ChromeOptions()
+  options.add_argument("--headless")
+  driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+  driver.get("https://demo.opencart.com/admin/")
+  op=driver.title
+  assert op=="Administration",'mismatched title'
