e511fc5b5076682547e8afe6954442570f33cad4
ramagihub <32742931+ramagihub@users.noreply.github.com>
2024-06-11 07:46:33 +0530

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+
 def test_1():
-  options = webdriver.ChromeOptions()
+  chrome_options = Options()
+  chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+  chrome_options.add_argument("--disable-gpu")  # Applicable to Windows to avoid error messages
+  chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
+  chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problem
   options.add_argument("--headless")
-  driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
   driver.get("https://demo.opencart.com/admin/")
   op=driver.title
   assert op=="Administration",'mismatched title'
