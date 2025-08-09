ad1daa2cfaecbe09a600dfa3fa3c0f50540e0785
ramagihub <32742931+ramagihub@users.noreply.github.com>
2025-08-09 08:23:56 +0530

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -5,14 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def test_1():
-  chrome_options = Options()
-  chrome_options.add_argument("--headless")  # Run Chrome in headless mode
-  chrome_options.add_argument("--disable-gpu")  # Applicable to Windows to avoid error messages
-  chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
-  chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problem
-  options.add_argument("--headless")
-  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-  driver.get("https://demo.opencart.com/admin/")
-  op=driver.title
-  assert op=="Administration",'mismatched title'
+  assert "Test"=="Test111"
+def test_2():
+  assert "rama"=="rama"
 
